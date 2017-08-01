db85416a4ad0ed27a56e42aa8a4fb3eae7b75883
Cameron O'Rourke <cameron.orourke@mac.com>
2017-07-31 17:01:49 -0700

    Trying an iframe in content area

--- pages/sales.js
+++ pages/sales.js
@@ -2,6 +2,6 @@ import App from "../components/App"
 
 export default (props) => (
   <App props={props} >
-    <h1>Sales Page</h1>
+    <iframe frameborder="0" src="https://secure.gooddata.com/reportWidget.html#project=/gdc/projects/r24k3k1e0sahveafix9276vujs3g011a&report=/gdc/md/r24k3k1e0sahveafix9276vujs3g011a/obj/7330&title=yes" width="600px" height="300px" allowTransparency="false"></iframe>
   </App>
 )
