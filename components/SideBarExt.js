93dcca7259e62c64db9296156e8802bdfbcf7358
levipearce <levi.pearce@gooddata.com>
2017-08-04 16:46:56 -0500

    Add trends drop down to external persona navigation

--- components/SideBarExt.js
+++ components/SideBarExt.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import {Col, Nav, NavItem, Glyphicon} from 'react-bootstrap'
+import {Col, Nav, NavItem, Glyphicon, Dropdown,MenuItem} from 'react-bootstrap'
 
 function handleSelect(selectedKey) {
   console.log('selected ' + selectedKey);
@@ -11,7 +11,16 @@ export default ({pathname}) => (
       <NavItem href="/external/"><Glyphicon bsStyle="larger" glyph="stats"/>Overview</NavItem>
       <NavItem href="/external/sales"><Glyphicon bsStyle="larger" glyph="usd"/>Sales</NavItem>
       <NavItem href="/external/customers"><Glyphicon bsStyle="larger" glyph="user"/>Customers</NavItem>
-      <NavItem href="/external/trends"><Glyphicon bsStyle="larger" glyph="th-large"/>Trends Test</NavItem>
+        <Dropdown>
+            <Dropdown.Toggle>
+            <Glyphicon glyph="th-large" />
+            Trends
+        </Dropdown.Toggle>
+            <Dropdown.Menu title="Trends">
+            <MenuItem href="/external/trends_overview.js" eventKey={3.1}>Trends Overview</MenuItem>
+            <MenuItem href="/external/trends.js" eventKey={3.1}>Product Trends</MenuItem>
+        </Dropdown.Menu>
+        </Dropdown>
     </Nav>
   </div>
 )
--- pages/external/trends_overview.js
+++ pages/external/trends_overview.js
@@ -1,7 +1,7 @@
 import App from "../../components/App"
 import DashboardNavHeader from "../../components/DashboardNavHeader"
 
-import {Breadcrumb,BreadcrumbItem, Grid,Row, Col} from 'react-bootstrap'
+import {Breadcrumb, Grid,Row, Col} from 'react-bootstrap'
 export default (props) => (
   <App pathname={props.url.pathname} >
       <Grid fluid>
@@ -11,23 +11,30 @@ export default (props) => (
                       Overview
                   </Breadcrumb.Item>
                   <Breadcrumb.Item active>
-                      Trends Overview
+                      Product Trends
                   </Breadcrumb.Item>
               </Breadcrumb>
           </Row>
           <Row>
               <Col sm={5}>
-                  <DashboardNavHeader title="Top 10 Activewear trends at GoodRetail" customClass="button-see-more"/>
+                  <DashboardNavHeader title="Trend Overview" customClass="button-see-more"/>
               </Col>
               <Col sm={5}>
                   Filters go here
               </Col>
           </Row>
           <Row>
-              <Col sm={4}>
+              <Col sm={10}>
                   <iframe frameBorder="0" src="https://whitelabeling.gooddata.com/dashboard.html#project=/gdc/projects/nrjs8u9m5y01o8b3584jrx8rosc0ynhw&dashboard=/gdc/md/nrjs8u9m5y01o8b3584jrx8rosc0ynhw/obj/9233" width="100%" height="380px" allowTransparency="false"></iframe>
               </Col>
-              <Col sm={6}>
+          </Row>
+          <Row>
+              <Col sm={10}>
+                  <DashboardNavHeader title="Top 3 Products" customClass="button-see-more"/>
+              </Col>
+          </Row>
+          <Row>
+              <Col sm={10}>
                   <iframe frameBorder="0" src="https://whitelabeling.gooddata.com/dashboard.html#project=/gdc/projects/nrjs8u9m5y01o8b3584jrx8rosc0ynhw&dashboard=/gdc/md/nrjs8u9m5y01o8b3584jrx8rosc0ynhw/obj/9233" width="100%" height="380px" allowTransparency="false"></iframe>
               </Col>
           </Row>
