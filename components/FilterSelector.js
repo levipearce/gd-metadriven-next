45ca5987397b9598e3eb92979440283e699080ac
Cameron O'Rourke <cameron.orourke@mac.com>
2017-08-08 15:20:31 -0700

    Working internal/index, some CSS things still needed

--- components/FilterSelector.js
+++ components/FilterSelector.js
@@ -1,7 +1,8 @@
 /**
  * Created by orourke on 8/4/17.
  *
- * <FilterSelector value={id} display={display} />
+ * <FilterSelector label="Text" items=[{periodItems}...] value={periodItems[1].value} onChange={this.handleFilterChange.bind(this)}
+ class="filter-right-align"/>
  */
 import React from 'react'
 import {Form, FormGroup, ControlLabel, FormControl, Button, Grid, Row, Col, Nav, NavItem, Glyphicon} from 'react-bootstrap'
@@ -10,19 +11,15 @@ export default class FilterSelector extends React.Component {
 
   constructor(props) {
     super(props)
-    console.log("constructor")
   }
 
   render() {
-    {
-      console.log("render(): ", this.state)
-    }
-
-      let customClass = this.props.class!==null? this.props.class: "class=\"filter-right-align\"";
+    let customClass = this.props.class !== null ? this.props.class : "class=\"filter-right-align\"";
     return <Form inline className={customClass}>
       <FormGroup controlId="subCatFilter">
         <ControlLabel className={this.props.controlLabelClass}>{this.props.label}:</ControlLabel>
-        <FormControl componentClass="select" placeholder="0" onChange={this.props.onChange}>
+        <FormControl componentClass="select" value={ this.props.value || '0'}
+                     onChange={this.props.onChange}>
           <option value="0">All</option>
           { this.props.items.map(item => <option value={item.value} key={item.value}>{item.display}</option>) }
         </FormControl>
--- pages/internal/index.js
+++ pages/internal/index.js
@@ -6,41 +6,80 @@ import FilterSelector from "../../components/FilterSelector"
 import PanelDismissable from "../../components/PanelDismissable"
 import {Button, ContextBox, Panel, Breadcrumb} from 'react-bootstrap'
 
-const buttonSeeMore = () =>
-  (<Button className="button-see-more" bsSize="small">See More</Button>)
+const periodItems = [
+  {value:"Q4 2017", display: "Q4 2017"},
+  {value:"Q3 2017", display: "Q3 2017"},
+  {value:"Q2 2017", display: "Q2 2017"},
+  {value:"Q1 2017", display: "Q1 2017"}
+]
 
-export default (props) => (
-  <App pathname={props.url.pathname}>
-    <Breadcrumb className="gd-custom-breadcrumb">
-      <Breadcrumb.Item active>
-        Overview
-      </Breadcrumb.Item>
-    </Breadcrumb>
+const gd_host = "https://whitelabeling.gooddata.com/dashboard.html"
+const gd_fragment = "project=/gdc/projects/nrjs8u9m5y01o8b3584jrx8rosc0ynhw&dashboard=/gdc/md/nrjs8u9m5y01o8b3584jrx8rosc0ynhw/obj/9233"
+const gd_attribute = "salesdate.aci81lMifn6q"
 
-    <DashboardFilterHeader title="Sales" path="/internal/sales" customClass="button-see-more">
-      <FilterSelector label="Select Period" items={[]} onChange={null} class="filter-right-align" />
-    </DashboardFilterHeader>
-    <PanelDismissable header="Alert" link="/internal/sales" bsStyle="info">
-      <p>Sales Performance for Activewear is down 10% from last year and is not on track to meet revenue targets.</p>
-    </PanelDismissable>
-    <ContentBox>
-      <iframe frameborder="0"
-              src="https://whitelabeling.gooddata.com/dashboard.html#project=/gdc/projects/nrjs8u9m5y01o8b3584jrx8rosc0ynhw&dashboard=/gdc/md/nrjs8u9m5y01o8b3584jrx8rosc0ynhw/obj/9233&tab=8bb5cbce4779&nochrome=true"
-              width="100%" height="340px" allowTransparency="true"></iframe>
-    </ContentBox>
+function generateURL(filter, tab) {
+  return `${gd_host}${filter}#${gd_fragment}&tab=${tab}&nochrome=true`
+}
 
-    <DashboardFilterHeader title="Product" button={buttonSeeMore} />
-    <ContentBox>
-      <iframe frameborder="0"
-              src="https://whitelabeling.gooddata.com/dashboard.html#project=/gdc/projects/nrjs8u9m5y01o8b3584jrx8rosc0ynhw&dashboard=/gdc/md/nrjs8u9m5y01o8b3584jrx8rosc0ynhw/obj/9233&tab=ab986eb84dbb&nochrome=true"
-              width="100%" height="360px" allowTransparency="true"></iframe>
-    </ContentBox>
+function generateTabs(filter) {
+  return {
+    embed_src_1: generateURL(filter, "8bb5cbce4779" ),
+    embed_src_2: generateURL(filter, "ab986eb84dbb" ),
+    embed_src_3: generateURL(filter, "a8cda3468f25" )
+  }
+}
 
-    <DashboardFilterHeader title="Customer" button={buttonSeeMore} />
-    <ContentBox>
-      <iframe frameborder="0"
-              src="https://whitelabeling.gooddata.com/dashboard.html#project=/gdc/projects/nrjs8u9m5y01o8b3584jrx8rosc0ynhw&dashboard=/gdc/md/nrjs8u9m5y01o8b3584jrx8rosc0ynhw/obj/9233&tab=a8cda3468f25&nochrome=true"
-              width="100%" height="300px" allowTransparency="true"></iframe>
-    </ContentBox>
-  </App>
-)
+export default class InternalIndex extends React.Component {
+
+  componentWillMount() {
+    this.setState(generateTabs(''))
+  }
+
+  handleFilterChange(e) {
+    let new_value = e.target.value
+    let filter = ( new_value != 0 ? `?${gd_attribute}=${new_value}` : '' )
+    this.setState(generateTabs(filter))
+  }
+
+  render() {
+    return <App pathname={this.props.url.pathname}>
+      <Breadcrumb className="gd-custom-breadcrumb">
+        <Breadcrumb.Item active>
+          Overview
+        </Breadcrumb.Item>
+      </Breadcrumb>
+
+      <DashboardFilterHeader title="Sales" path="/internal/sales" customClass="button-see-more">
+        <FilterSelector label="Select Period" items={periodItems} value={periodItems[1].value} onChange={this.handleFilterChange.bind(this)}
+                        class="filter-right-align"/>
+      </DashboardFilterHeader>
+      <PanelDismissable header="Alert" link="/internal/sales" bsStyle="info">
+        <p>Sales Performance for Activewear is down 10% from last year and is not on track to meet revenue targets. <a
+          href="/internal/sales">See more details</a></p>
+      </PanelDismissable>
+      <ContentBox>
+        <iframe frameborder="0"
+                src={this.state.embed_src_1}
+                width="100%" height="340px" allowTransparency="true"></iframe>
+      </ContentBox>
+
+      <DashboardFilterHeader title="Product" path="/internal/producdts" customClass="button-see-more"/>
+      <ContentBox>
+        <iframe frameborder="0"
+                src={this.state.embed_src_2}
+                width="100%" height="360px" allowTransparency="true"></iframe>
+      </ContentBox>
+
+      <PanelDismissable header="Alert" link="/internal/customers" bsStyle="info">
+        <p>Similar brands are showing a downtrend for this demographic. <a href="/internal/customers">See more
+          details</a></p>
+      </PanelDismissable>
+      <DashboardFilterHeader title="Customer" path="/internal/customers" customClass="button-see-more"/>
+      <ContentBox>
+        <iframe frameborder="0"
+                src={this.state.embed_src_3}
+                width="100%" height="300px" allowTransparency="true"></iframe>
+      </ContentBox>
+    </App>
+  }
+}
