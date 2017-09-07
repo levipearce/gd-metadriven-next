4dd7da6b3a73cb284843a58db04fc08759d1e8b2
levipearce <levi.pearce@gooddata.com>
2017-09-07 14:20:47 -0500

    Adding LCM Layout

---
+++ pages/lcm/index.js
@@ -0,0 +1,48 @@
+import AppFS from "../../components/AppMetaDriven"
+import {Row,Col, ContextBox,Button, DropdownButton, MenuItem} from 'react-bootstrap'
+import React from 'react'
+import ProvisionCustomerModal from "../../components/lcm/ProvisionCustomerModal";
+import Cookies from "universal-cookie";
+export default class LCMIndex extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    onSelect(eventKey){
+    }
+
+    render() {
+        const cookies = new Cookies();
+        let count = cookies.get('prod_us_premium_count');
+        return <AppFS pathname={this.props.url.pathname}>
+                      <Row>
+
+                          <Col className="lcm-header" sm={2}></Col>
+                          <Col className="lcm-header" sm={2}>Development <Button className="button-options pull-right">...</Button></Col>
+                          <Col className="lcm-header" sm={2}>QA <Button className="button-options pull-right">...</Button></Col>
+                          <Col className="lcm-header" sm={2}>Production US  <DropdownButton noCaret className="button-options pull-right"  title="..." onSelect={this.onSelect}>
+                              <MenuItem eventKey="1">Add Customer</MenuItem>
+                              <MenuItem eventKey="2"><ProvisionCustomerModal message="Provision Customer" domain="Production US"/></MenuItem>
+                              <MenuItem divider />
+                              <MenuItem eventKey="3">Release from QA</MenuItem>
+                              <MenuItem eventKey="4">Revert to Last Release</MenuItem>
+                          </DropdownButton></Col>
+                          <Col className="lcm-header" sm={2}>Production EU <Button className="button-options pull-right">...</Button></Col>
+                      </Row>
+        <Row>
+            <Col className="lcm-segment" sm={2}><Button className="button-see-more">Basic</Button></Col>
+            <Col className="lcm-domain-segment" sm={2}><p>7 Clients<br/> Last Changed 2m ago</p></Col>
+            <Col className="lcm-domain-segment" sm={2}></Col>
+            <Col className="lcm-domain-segment" sm={2}></Col>
+            <Col className="lcm-domain-segment" sm={2}></Col>
+        </Row>
+        <Row>
+            <Col className="lcm-segment" sm={2}><Button className="button-see-more">Premium</Button></Col>
+            <Col className="lcm-domain-segment" sm={2}><p>7 Clients<br/> Last Changed 2m ago</p></Col>
+            <Col className="lcm-domain-segment" sm={2}></Col>
+            <Col className="lcm-domain-segment" sm={2}><p>{count} Clients<br/> Last Changed 2m ago</p></Col>
+            <Col className="lcm-domain-segment" sm={2}><p>706 Clients<br/> Last Changed 2m ago</p></Col>
+        </Row>
+
+        </AppFS>
+    }
+}
