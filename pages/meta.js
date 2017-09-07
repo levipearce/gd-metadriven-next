ad49f872ba4c1839151517e914ce0a1c4813f3bb
levipearce <levi.pearce@gooddata.com>
2017-09-07 14:20:35 -0500

    Adding LCM Layout

--- pages/meta.js
+++ pages/meta.js
@@ -3,8 +3,20 @@
  *  Paths represent individual persona based stories.
  */
 export const LAYOUTS = [
-      {NAME: 'RETAIL_INTERNAL', NAVPATH: "/internal/", STYLEPATH:"internal", USERNAME:'Alan Smith', HEADER: "",SIDEBAR:""},
-      {NAME: 'RETAIL_EXTERNAL', NAVPATH: "/external/", STYLEPATH:"external",  USERNAME:'Alan Smith', HEADER: "",SIDEBAR:""},
+      {NAME: 'RETAIL_INTERNAL', NAVPATH: "/internal/", STYLEPATH:"internal", PRODUCT:'Womens Active Wear',USERNAME:'Alan Smith',  AVATAR:'67',TITLE:'Regional Manager',
+          NAVMENU:[
+              {text:'Overview',navpath:'/internal',glyph:'warning-sign'},
+              {text:'Sales',navpath:'/internal/sales',glyph:'user'},
+              {text:'Customers',navpath:'/internal/customers',glyph:'th-large'},
+              {text:'Products',navpath:'/internal/products',glyph:'th-large'}
+          ]},
+      {NAME: 'RETAIL_EXTERNAL', NAVPATH: "/external/", STYLEPATH:"external",  USERNAME:'Alan Smith',  AVATAR:'67',TITLE:'Regional Manager',
+          NAVMENU:[
+              {text:'Overview',navpath:'/external',glyph:'warning-sign'},
+              {text:'Sales',navpath:'/external/sales',glyph:'user'},
+              {text:'Customers',navpath:'/external/customers',glyph:'user'},
+              {text:'Trends',navpath:'/external/trends',glyph:'th-large'}
+          ]},
       {NAME: 'FS_BASIC', NAVPATH: "/fsbasic", STYLEPATH:"internal", PRODUCT:'Asset Management System', USERNAME:'Brian Moody', AVATAR:'67',TITLE:'Regional Manager',
           NAVMENU:[
               {text:'Alerts',navpath:'/fsbasic',glyph:'warning-sign'},
@@ -18,6 +30,9 @@ export const LAYOUTS = [
               {text:'Market Analysis',navpath:'/fsprem/markets',glyph:'user'},
               {text:'Trends',navpath:'/fsprem/trends',glyph:'th-large'},
               {text:'Analyze',navpath:'/fsprem/analyze',glyph:'eye-open'}
-      ]}
+      ]},
+    {NAME: 'LCM', NAVPATH: "/lcm", STYLEPATH:"lcm", PRODUCT:'Lifecycle Management System', USERNAME:'Sam Smith', AVATAR:'67',TITLE:'Deployment Manager',
+        NAVMENU:[
+        ]}
 ];
 
